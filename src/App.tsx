f02366d9654bc88c0fb2671e615c1f6f9770f0d9
BerislavV <berislav.vidakovic@gmail.com>
2025-12-25 17:47:03 +0100

    Fixed field mapping and adjusted cell alignment

--- src/App.tsx
+++ src/App.tsx
@@ -68,14 +68,14 @@ return (
           <div className="grid"
                style={{ gridTemplateColumns: currentColumns().join(' ') }}
           >
-            <div className="grid-header">ID</div>
+            <div className="grid-header cell-center">ID</div>
             <div className="grid-header">Name</div>
             <div className="grid-header">City</div>
             <div className="grid-header">Country</div>
 
             {customers.map(c => (
               <div className="grid-row" key={c.id}>
-                <div>{c.id}</div>
+                <div className="cell-center">{c.id}</div>
                 <div>{c.name}</div>
                 <div>{c.city}</div>
                 <div>{c.country}</div>
@@ -89,19 +89,19 @@ return (
           <div className="grid"
                style={{ gridTemplateColumns: currentColumns().join(' ') }}
           >
-            <div className="grid-header">ID</div>
+            <div className="grid-header cell-center">ID</div>
             <div className="grid-header">Name</div>
             <div className="grid-header">Description</div>
             <div className="grid-header">Manufacturer</div>
-            <div className="grid-header">Price USD</div>
+            <div className="grid-header cell-right">Price USD</div>
 
             {products.map(p => (
               <div className="grid-row" key={p.id}>
-                <div>{p.id}</div>
+                <div className="cell-center">{p.id}</div>
                 <div>{p.name}</div>
                 <div>{p.description}</div>
                 <div>{p.manufacturer}</div>
-                <div>{p.priceUSD}</div>
+                <div className="cell-right">{p.priceUSD}</div>
               </div>
             ))}
           </div>        
@@ -120,17 +120,17 @@ return (
         {/* Order overview table */}
         <div className="grid-container">
           <div className="grid grid-order">
-            <div className="grid-header">ID</div>
+            <div className="grid-header cell-center">ID</div>
             <div className="grid-header">Customer</div>
-            <div className="grid-header">Products</div>
-            <div className="grid-header">Amount USD</div>
+            <div className="grid-header cell-center">Products</div>
+            <div className="grid-header cell-right">Amount USD</div>
 
             {ordersMaster.map(o => (
               <div className="grid-row" key={o.id}>
-                <div>{o.id}</div>
+                <div className="cell-center">{o.id}</div>
                 <div>{o.customer}</div>
-                <div>{o.products}</div>
-                <div>{o.amountUSD}</div>
+                <div className="cell-center">{o.products}</div>
+                <div className="cell-right">{o.amountUSD}</div>
               </div>
             ))}
           </div>
