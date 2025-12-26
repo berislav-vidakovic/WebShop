292dfb2c2e3a42a1d122d8abd258f25703ec40af
BerislavV <berislav.vidakovic@gmail.com>
2025-12-26 15:42:35 +0100

    Pagination update

--- src/Pagination.tsx
+++ src/Pagination.tsx
@@ -1,4 +1,3 @@
-
 export const Pagination = ({
   currentPage,
   totalPages,
--- src/views.ts
+++ src/views.ts
@@ -2,7 +2,6 @@ import type { Customer, Product, OrderItem } from './interfaces';
 
 import type { TableType } from "./interfaces";
 
-
 // Generic function to compute paginated data and column widths
 export const getTableView = (
   table: TableType,
@@ -15,34 +14,40 @@ export const getTableView = (
   let data: any[] = [];
   let columnWidths: number[] = [];
 
+  let sorted;
   if (table === "Customers") {
-    const sorted = [...customers].sort((a,b) => a.id - b.id);
-    data = sorted.slice((currentPage-1)*rowsPerPage, currentPage*rowsPerPage);
+    sorted = [...customers].sort((a,b) => a.id - b.id);
     columnWidths = [
-      Math.max(...customers.map(c => String(c.id).length)),
-      Math.max(...customers.map(c => c.name.length)),
-      Math.max(...customers.map(c => c.city.length)),
-      Math.max(...customers.map(c => c.country.length)),
+      Math.max(...customers.map(c => String(c.id).length), "ID".length ),
+      Math.max(...customers.map(c => c.name.length), "Name".length ),
+      Math.max(...customers.map(c => c.city.length), "City".length ),
+      Math.max(...customers.map(c => c.country.length), "Country".length ),
     ];
   } else if (table === "Products") {
-    const sorted = [...products].sort((a,b) => a.id - b.id);
-    data = sorted.slice((currentPage-1)*rowsPerPage, currentPage*rowsPerPage);
+    sorted = [...products].sort((a,b) => a.id - b.id);
     columnWidths = [
-      Math.max(...products.map(p => String(p.id).length)),
-      Math.max(...products.map(p => p.name.length)),
-      Math.max(...products.map(p => p.description.length)),
-      Math.max(...products.map(p => p.manufacturer.length)),
-      Math.max(...products.map(p => String(p.priceUSD).length)),
+      Math.max(...products.map(p => String(p.id).length), "ID".length ),
+      Math.max(...products.map(p => p.name.length), "Name".length ),
+      Math.max(...products.map(p => p.description.length), "Description".length ),
+      Math.max(...products.map(p => p.manufacturer.length), "Manufacturer".length ),
+      Math.max(...products.map(p => String(p.priceUSD).length), "Price USD".length ),
     ];
   } else if (table === "Orders") {
-    const sorted = [...orderItems].sort((a,b) => a.id - b.id);
-    data = sorted.slice((currentPage-1)*rowsPerPage, currentPage*rowsPerPage);
+    sorted = [...orderItems].sort((a,b) => a.id - b.id);
     columnWidths = [
-      Math.max(...orderItems.map(o => String(o.id).length)),
-      Math.max(...orderItems.map(o => o.customer.length)),
-      Math.max(...orderItems.map(o => o.product.length)),
-      Math.max(...orderItems.map(o => String(o.quantity).length)),
+      Math.max(...orderItems.map(o => String(o.id).length), "Order ID".length ),
+      Math.max(...orderItems.map(o => o.customer.length), "Customer".length ),
+      Math.max(...orderItems.map(o => o.product.length), "Product".length ),
+      Math.max(...orderItems.map(o => String(o.quantity).length), "Quantity".length ),
     ];
+  }
+  if( sorted ){
+    data = sorted.slice((currentPage-1)*rowsPerPage, currentPage*rowsPerPage);
+    console.log( (currentPage-1)*rowsPerPage, currentPage*rowsPerPage, data.length );
+    
+    let first = (currentPage-1)*rowsPerPage;
+    while( currentPage > 1 && data.length < rowsPerPage )
+      data = sorted.slice(--first, currentPage*rowsPerPage);
   }
 
   return { paginatedData: data, columnWidths };
